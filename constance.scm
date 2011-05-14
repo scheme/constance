600f79167494aa6b31fb1be744bb37db7867bd87
Daniel King <danking@ccs.neu.edu>
2011-05-14 18:55:11 -0400

    remove an ambiguity in the DEFINE-CONSTANCE macro syntax

--- constance.scm
+++ constance.scm
@@ -2,18 +2,20 @@
   (lambda (e rename c)
     (let* ((alist-name (cadr e))
            (c-headers  (caddr e))
-           (pairs      (cdddr e))
+           (pairs      (cadddr e))
            (results    (get-value-pairs-from-c alist-name c-headers pairs)))
       `(,(rename 'begin)
         ,@(map (lambda (pair)
                  `(,(rename 'define) ,(first pair) ,(second pair)))
                results)))))
 
-;; (define-constance IDENTIFIER [LISTOF SYMBOL] [ASSOC-LIST SYMBOL STRING])
-;; (define-constance alist-name c-headers (list (id . c-macro) ...))
+;; (define-constance alist-name
+;;   (headers ...)
+;;   ((id c-macro)
+;;    (id2 c-macro2)
+;;    ...))
 ;; =>
-;; (define alist-name
-;;   (list (id . value)
-;;         ...))
+;; (begin (define id value)
+;;        (define id2 value2))
 ;; where value is the value of c-macro in the environment of a c program with
 ;; the header files specified by c-headers
