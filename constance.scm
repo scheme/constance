c90c3235415332071bd2f92957cf5d0f2028a772
Daniel King <danking@ccs.neu.edu>
2011-08-16 22:32:45 -0400

    fixed a misleading identifier and added a warning note
     - alist-name is a vestige of when we planned to define an alist with the
       pairs bound in there, I've renamed it to something less suggestive
     - adding a warning note cautioning against non-unique constance-names

--- constance.scm
+++ constance.scm
@@ -1,16 +1,16 @@
 (define-syntax define-constance
   (lambda (e rename c)
-    (let* ((alist-name (cadr e))
+    (let* ((constance-name (cadr e))
            (c-headers  (caddr e))
            (pairs      (cadddr e))
-           (results    (get-value-pairs-from-c alist-name c-headers pairs)))
+           (results    (get-value-pairs-from-c constance-name c-headers pairs)))
       `(,(rename 'begin)
         ,@(map (lambda (pair)
                  `(,(rename 'define) ,(string->symbol (first pair))
                                      ,(second pair)))
                results)))))
 
-;; (define-constance alist-name
+;; (define-constance constance-name
 ;;   (headers ...)
 ;;   ((id c-macro)
 ;;    (id2 c-macro2)
@@ -20,3 +20,7 @@
 ;;        (define id2 value2))
 ;; where value is the value of c-macro in the environment of a c program with
 ;; the header files specified by c-headers
+
+;; NOTE: constance-name MUST BE UNIQUE within a project (specifically, amongst
+;; any DEFINE-CONSTANCE forms which have the same working directory at macro
+;; expansion time
